1626a9d3cd4e7f09251a0adc1ee5bfbe85ebe80a
ayuanjm <2459087172@qq.com>
2022-04-09 13:56:40 +0800

    add one class

---
+++ oa/src/main/java/GitTest.java
@@ -0,0 +1,10 @@
+ /**
+ * Copyright(c) 2018 Sunyur.com, All Rights Reserved. Author: YuanJiaMin Create date: 2022/4/9
+ */
+package PACKAGE_NAME;/** 
+ * 
+ * @author YuanJiaMin
+ * @date 2022/4/9 13:49
+ */
+public class GitTest {
+}
