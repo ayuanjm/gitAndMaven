3304752f0d45e12a3a635c77bf436ad844801289
ayuanjm <2459087172@qq.com>
2022-04-09 14:01:43 +0800

    git test

--- oa/src/main/java/com/git/temp/GitTest.java
+++ oa/src/main/java/com/git/temp/GitTest.java
@@ -1,10 +1,13 @@
- /**
+package com.git.temp; /**
  * Copyright(c) 2018 Sunyur.com, All Rights Reserved. Author: YuanJiaMin Create date: 2022/4/9
  */
-package PACKAGE_NAME;/** 
- * 
+
+/**
  * @author YuanJiaMin
  * @date 2022/4/9 13:49
  */
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
 }
